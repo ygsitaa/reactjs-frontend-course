9faa0a1608bbbeee924e2692f1c607f2e2787467
Daniel Hodiamont <213696653+ygsitaa@users.noreply.github.com>
2026-02-03 00:47:11 +0100

    changed twitter link to github link in footer

--- reactjs-tutorial/src/components/layouts/Layout.jsx
+++ reactjs-tutorial/src/components/layouts/Layout.jsx
@@ -12,7 +12,7 @@ export default function Layout(props) {
     const footer = (
         <footer>
             <small>Created by</small>
-            <a target="_blank" href="https://twitter.com/ygsitaa">
+            <a target="_blank" href="https://github.com/ygsitaa">
             <img alt="pfp" src="https://pbs.twimg.com/profile_images/1903147820699496449/kpvh59VF_400x400.jpg"></img>
                 <p>ygsitaa</p>
                 <i className="fa-brands fa-twitter"></i>
